64ff28f2c228590dd3b02daaf007e2042db92562
alishaneelu <alisha.sngh15@gmail.com>
2022-05-09 19:50:43 +0530

    Callback url updated

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
     url: 'iam-udacity.us', // the auth0 domain prefix
     audience: 'coffee shop', // the audience set for the auth0 app
     clientId: 'mLdqTju0F6z6G4bwwD9CMIblNTI9tJ75', // the client id generated for the auth0 app
-    //callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-    callbackURL: 'https://127.0.0.1:8080', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    //callbackURL: 'https://127.0.0.1:8080', // the base url of the running ionic application. 
   }
 };
