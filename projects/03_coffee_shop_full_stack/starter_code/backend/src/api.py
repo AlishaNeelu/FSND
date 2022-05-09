2ae8826c7605e2be0e5872438b3b092d07b82838
alishaneelu <alisha.sngh15@gmail.com>
2022-05-09 13:00:43 +0530

    IAM Changes

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -17,7 +17,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this function will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -28,7 +28,17 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks')
+def get_drinks():
+    drinks_list = Drink.query.all()
 
+    if len(drinks_list) == 0:
+        abort(404)
+    short_drink = [drink.short() for drink in drinks_list]
+    return jsonify({
+        'success': True,
+        'drinks': short_drink
+    })
 
 '''
 @TODO implement endpoint
@@ -38,7 +48,20 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drinks_details(jwt): 
+    drinks_list = Drink.query.all()
 
+    if len(drinks_list) == 0:
+        abort(404)
+
+    long_drink = [drink.long() for drink in drinks_list]
+
+    return jsonify({
+        'success': True,
+        'drinks': long_drink
+    })
 
 '''
 @TODO implement endpoint
@@ -49,7 +72,21 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks',methods=['POST'])
+@requires_auth('post:drinks')
+def create_drinks(jwt): 
+    body = request.get_json()
+    if 'title' not in body:
+        abort(422)
+    new_drink = Drink(title=body.get('title'), recipe=json.dumps(body.get('recipe')))
+    new_drink.insert()
 
+    long_drink = [new_drink.long()]
+
+    return jsonify({
+        'success': True,
+        'drinks': long_drink
+    })
 
 '''
 @TODO implement endpoint
@@ -62,7 +99,31 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def modify_drink(jwt, id):
+    filtered_drink = Drink.query.get(id)
+    if filtered_drink is None:
+        abort(404)
+    
+    body = request.get_json()
+    if 'title' not in body:
+        abort(422)
 
+    title = body.get('title')
+
+    if 'title' in body:
+        filtered_drink.title = body['title']
+    if 'recipe' in body:
+        filtered_drink.recipe = json.dumps(body['recipe'])
+ 
+    filtered_drink.update()
+    long_drink = [filtered_drink.long()]
+
+    return jsonify({
+        'success': True,
+        'drinks': long_drink
+    })
 
 '''
 @TODO implement endpoint
@@ -74,7 +135,19 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drinks(jwt, id):
+    delete_drink = Drink.query.get(id)
+    if delete_drink is None:
+        abort(404)
 
+    delete_drink.delete()
+
+    return jsonify({
+        'success': True,
+        'delete': delete_drink.id
+    })
 
 # Error Handling
 '''
@@ -101,18 +174,41 @@ def unprocessable(error):
                     }), 404
 
 '''
-
+@app.errorhandler(404)
+def resource_not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "unauthorized"
+    }), 401
 
+@app.errorhandler(400)
+def resource_not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+@app.errorhandler(AuthError)
+def handle_authentication_error(error):
+    response = jsonify(error.error)
+    response.status_code = error.status_code
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    return response
+
--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,10 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'iam-udacity.us', // the auth0 domain prefix
+    audience: 'coffee shop', // the audience set for the auth0 app
+    clientId: 'mLdqTju0F6z6G4bwwD9CMIblNTI9tJ75', // the client id generated for the auth0 app
+    //callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://127.0.0.1:8080', // the base url of the running ionic application. 
   }
 };
